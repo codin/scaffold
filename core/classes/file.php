d1a9ae323129bfcf36c9fed7022df80f03947498
Craig <bertycrazy543@hotmail.co.uk>
2012-08-23 13:20:20 +0100

    Added some detailed comments.
    We should be doing this to make it easier.

--- core/classes/file.php
+++ core/classes/file.php
@@ -3,7 +3,11 @@
 class File {
 	
 	/**
-	 *	File::get('test.txt'); or File::get('test.txt', 10, 'path/to/file');
+	 *	@desc Get a file
+	 *  @param Filename
+	 *  @param Character Limit
+	 *	@param Path/URL
+	 *  @return Array 
 	 */
 	static function get($name, $char_limit = false, $url = false) {
 		
@@ -33,11 +37,53 @@ class File {
 		return false;
 	}
 	
+	/**
+	 *	@desc Delete a file
+	 *  @param Filename
+	 *  @param Path
+	 *  @return Boolean 
+	 */
 	static function delete() {
-	
+		// TODO Make this function delete a file.
 	}
 	
+	/**
+	 *	@desc Rename a file
+	 *  @param Filename
+	 *  @param Path
+	 *  @return Boolean 
+	 */
 	static function rename() {
-		
+		// TODO Make this function rename a file.
+	}
+	
+	/**
+	 *	@desc Edit a file
+	 *  @param Filename
+	 *  @param Path
+	 *  @return String 
+	 */
+	static function edit() {
+		// TODO Make this function edit contents of a text-based (e.g. no image or software specific file)
+	}
+	
+	/**
+	 *	@desc Upload a file
+	 *  @param Temp Location
+	 *  @param Destination
+	 *  @return Boolean 
+	 */
+	static function upload() {
+		// TODO Make this function upload a file to a folder from tmp
+	}
+	
+	/**
+	 *	@desc Move a file from temp location
+	 *  @param Temp location
+	 *  @param Destination
+	 *  @return Boolean 
+	 */
+	private static function _move_from_tmp() {
+		// TODO Move file from temp as part of upload
 	}
 }
