26d8e9245b710a5d6ae803a3d53d57e4c3028f0a
Craig <bertycrazy543@hotmail.co.uk>
2012-08-26 22:47:30 +0100

    Got rid of my debugging stuff i left in

--- app/controllers/main.php
+++ app/controllers/main.php
@@ -4,10 +4,6 @@ class Main_controller extends Controller {
     public function __construct() {
         parent::__construct();
         
-		Session::set('test', array('hello', 'goodbye'));
-		dump($_SESSION['test']);
-		//dump(Session::get('test'));
-        
         //  Set template data
         $this->template->set(array(
             'language' => Config::get('language'),
@@ -15,11 +11,10 @@ class Main_controller extends Controller {
             'test' => 'This is a just a test.'
         ));
 		
-		/*
         //  Oh wait, this is totally an AJAX request
         $this->ajax->output(array(
             'content' => $this->template->render() . load_time()
-        ));*/
+        ));
     }
     
     public function index() {
