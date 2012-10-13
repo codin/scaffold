afb0bb4b3246402a3d96bcca6f989d0e2460fb3a
Visual Idiot <iam@visualidiot.com>
2012-10-13 22:37:03 +0100

    better inputs

--- core/classes/input.php
+++ core/classes/input.php
@@ -4,10 +4,10 @@ class Input {
 
 	public static function posted($what = false) {
 	
-	    if(!empty($what)) {
+	    if($what !== false) {
             if(is_array($what)) {
                 foreach($what as $field) {
-                    if(!isset($_POST[$field]) and !empty($_POST[$field])) return false;
+                    if(!isset($_POST[$field]) or empty($_POST[$field])) return false;
                 }
                 
                 return true;
@@ -19,12 +19,28 @@ class Input {
 		return count($_POST) > 0;
 	}
 
-	public static function get($var, $fallback = '') {
+	public static function get($var = false, $fallback = '') {
+	    if($var === false) {
+	        foreach($_GET as $key => $value) {
+	            self::get($key);
+	        }
+	    }
+	    
 		return self::_receive($_GET, $var, $fallback);
 	}
 
 	public static function post($var, $fallback = '') {
+	    if($var === false) {
+	        foreach($_POST as $key => $value) {
+	            self::post($key);
+	        }
+	    }
+	    
 		return self::_receive($_POST, $var, $fallback);
+	}
+	
+	public static function server($var, $fallback = '') {
+		return self::_receive($_SERVER, strtoupper($var), $fallback);
 	}
 	
 	public static function escape($str) {
