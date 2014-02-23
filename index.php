de2f6a35fd1c3bfcc5b4183d5eecfc6c3e230f0f
Visual Idiot <iam@visualidiot.com>
2014-02-23 17:03:11 +0000

    shufflin'

--- index.php
+++ index.php
@@ -1,10 +1,39 @@
-<?php
+<?php 
 
 /**
- *	Scaffold v0.1.1
- *	by the Codin' Co.
+ *   Scaffold, a tiny PHP webapp framework.
  *
- *	Load up the index
+ *   @author	Codin' Co. <hi@codin.co>
+ *   @version	2.0.0-alpha
+ *   @package	Scaffold
+ *
+ *   This is just a sample usage for Scaffold. Adjust for taste.
  */
  
-include_once 'html/index.php';
+ 
+/**
+ *   Load our main Scaffold file. We'll load more in later.
+ */
+require 'scaffold/init.php';
+
+/**
+ *   Set up our Scaffold object as $app.
+ *   We can give Scaffold options three ways:
+ *    1. Passing an array of options as parameters.
+ *	  2. Passing the name of a file as a string parameter.
+ *	  3. Passing the name of a directory as a string parameter.
+ */
+$app = new \Scaffold\Scaffold('app/config');
+
+
+/**
+ *   We'll include a directory of routes instead of muddying up
+ *   our main file, so we can call the routes() method to auto-
+ *   load an entire folder of routes.
+ */
+$app->routes('app/routes');
+
+/**
+ *   And start the application!
+ */
+$app->run();
