f5cc44a08085674d01c7e3aadcd78a9a4ac33dcd
Craig Childs <childscraig17@gmail.com>
2016-12-12 22:25:13 +0000

    Document and separate the services in the bootstrap file

--- system/boostrap.php
+++ system/boostrap.php
@@ -6,18 +6,27 @@ include_once 'helpers.php';
 // Create the new app instance with our
 // service instances
 $app = new Scaffold\Foundation\App(__DIR__ . '/../', [
+    
+    // Services required in order for Scaffold
+    // to function correctly.
     'config'     => new Scaffold\Foundation\Config(),
-    'database'   => new Illuminate\Database\Capsule\Manager(),
-    'logger'     => new Monolog\Logger('scaffold'),
     'request'    => new Scaffold\Http\Request(),
     'response'   => new Scaffold\Http\Response(),
     'router'     => new Scaffold\Http\Router(),
     'stopwatch'  => new Symfony\Component\Stopwatch\Stopwatch(),
+
+    // Optional Services which can be 
+    // disabled or altered.
+    'database'   => new Illuminate\Database\Capsule\Manager(),
+    'logger'     => new Monolog\Logger('scaffold'),
     'templater'  => new Symfony\Component\Templating\DelegatingEngine(),
     'dispatcher' => new Symfony\Component\EventDispatcher\EventDispatcher(),
     'cache'      => new Scaffold\Caching\Cache(
         new Scaffold\Caching\Adapters\FileCacheAdapter()
     ),
+    
+    // Your custom application services 
+    // can go below here:
 ]);
 
 // Include our routes file
