2ac8635bf52c1ec87f47061d0841312a4a9aa181
Craig Childs <childscraig17@gmail.com>
2017-01-07 18:07:21 +0000

    Setup gulp stuff

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,48 @@
-var gulp = require('gulp');
+var gulp        = require('gulp');
+var boilerplate = require('gulp-boilerplate')(gulp);
 
-gulp.task('css', function() {
+// Styles task
+gulp.task('styles', boilerplate.styles({
+    src: 'src/styles/**/*.scss',
+    public: 'public/assets/css',
+}));
 
-});
+// Scripts task
+gulp.task('scripts', boilerplate.scripts({
+    src: 'src/js/**/*.js',
+    filename: 'main.min.js',
+    public: 'public/assets/js',
+}));
 
-gulp.task('js', function() {
+// Images task
+gulp.task('images', boilerplate.images({
+    src: 'src/img/**/*',
+    public: 'public/assets/img',
+}));
 
-});
+// Clean task
+gulp.task('clean', boilerplate.clean({
+    paths: [
+        'public/assets/css',
+        'public/assets/js',
+        'public/assets/img'
+    ],
+}));
 
-gulp.task('img', function() {
+// Clear task
+gulp.task('clear', boilerplate.clear());
 
-});
+// Watch task
+gulp.task('watch', boilerplate.watch({
+    tasks: [
+        {path: 'src/styles/**/*.scss', tasks: ['styles']},
+        {path: 'src/js/**/*.js', tasks: ['scripts']},
+        {path: 'src/img/**/*', tasks: ['images']}
+    ]
+}));
 
-gulp.task('default', ['css', 'js', 'img']);
+// Default task
+gulp.task('default', boilerplate.standard());
+
+// Production task
+gulp.task('production', ['clean'], boilerplate.production());
