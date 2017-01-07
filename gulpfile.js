03fcda8f9cd1ffb238666a898c43a050d547d979
Craig Childs <childscraig17@gmail.com>
2017-01-07 17:19:54 +0000

    Pull in gulp and boilerplate. Update docker and git ignore files

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+
+gulp.task('css', function() {
+
+});
+
+gulp.task('js', function() {
+
+});
+
+gulp.task('img', function() {
+
+});
+
+gulp.task('default', ['css', 'js', 'img']);
